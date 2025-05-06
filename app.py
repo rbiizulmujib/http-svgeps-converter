c69c23a60a361324199ead4ada3bc2190afebd00
rbiizulmujib <37804178+rbiizulmujib@users.noreply.github.com>
2025-05-06 09:44:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,5 +30,7 @@ def convert_to_eps():
         if os.path.exists(eps_path):
             os.remove(eps_path)
 
+
 if __name__ == '__main__':
-    app.run(port=3000)
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host='0.0.0.0', port=port)
